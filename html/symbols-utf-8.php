5149c33c8168b0d13c28ed3b7099345d3be95a38
iadfeshchm <zlatov@ya.ru>
2015-11-01 18:40:06 +0300

    html - utf8 symbols

---
+++ html/symbols-utf-8.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>symbols utf-8</title>
+	<style>
+	body {
+		font-size: 16px;
+	}
+	</style>
+</head>
+<body>
+<pre>
+<?php 
+for ($i=9472; $i < 9599 ; $i++) { 
+	echo "&#$i;         #$i\n";
+}
+?>
+</pre>
+</body>
+</html>
+
+
