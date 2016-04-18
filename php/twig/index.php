d8e2331ad01ac79a380d3872e115050cb4026ffb
iadfeshchm <zlatov@ya.ru>
2016-04-18 21:29:19 +0300

    test twig

---
+++ php/twig/index.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once(__DIR__ . '/vendor/autoload.php');
+
+/*
+// 1
+$loader = new Twig_Loader_String();
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('Hello {{ name }}!', array('name' => 'Fabien'));
+*/
+
+
+// 2
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/tpls');
+$twig = new Twig_Environment($loader, array(
+    'cache'       => 'compilation_cache',
+    'auto_reload' => true
+));
+
+$content = [
+	'0',
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+];
+
+echo $twig->render('content.html',['content'=>$content,'projectName' => 'Twig PHP']);
---
+++ php/twig/tpls/testphp.php
@@ -0,0 +1,4 @@
+<p>html</p>
+<!-- php do not work in twig tpl -->
+<?php echo "string"; ?>
+<pre>php do not work in twig tpl <?php print_r($foo); ?></pre>
