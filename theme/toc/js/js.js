68ce62ff4e4a822f805a2d428d63bf35e9a80636
iadfeshchm <zlatov@ya.ru>
2016-08-27 15:07:39 +0300

    to toc

--- theme/toc/js/js.js
+++ theme/toc/js/js.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-    $("#toc").append('<p>Содержание:</p>');
-    $("#toc").append('<ul></ul>');
+    // $("#toc").append('<p>Содержание:</p>');
+    $("#toc").append('<ul class="nav nav-pills nav-stacked"></ul>');
     $("h2").each(function(i) {
         var current = $(this);
         current.attr("id", "h2" + i);
         $("#toc").children("ul").append("<li><a id='linkh2" + i + "' href='#h2" +
             i + "' title='" + current.attr("tagName") + "'>" +
-            current.html() + "</a><ul></ul></li>");
+            current.html() + '</a><ul class="nav nav-pills nav-stacked"></ul></li>');
     });
     $("h3").each(function(i) {
         var current = $(this);
@@ -15,7 +15,7 @@ $(document).ready(function() {
         var j = prevh2.attr("id").substring(2);
         $("#linkh2" + j).next("ul").append("<li><a id='linkh3" + i + "' href='#h3" +
             i + "' title='" + current.attr("tagName") + "'>" +
-            current.html() + "</a><ul></ul></li>");
+            current.html() + '</a><ul class="nav nav-pills nav-stacked"></ul></li>');
     });
     $("h4").each(function(i) {
         var current = $(this);
@@ -25,7 +25,7 @@ $(document).ready(function() {
         alert
         $("#linkh3" + j).next("ul").append("<li><a id='linkh4" + i + "' href='#h4" +
             i + "' title='" + current.attr("tagName") + "'>" +
-            current.html() + "</a><ul></ul></li>");
+            current.html() + '</a><ul class="nav nav-pills nav-stacked"></ul></li>');
     });
     $("h5").each(function(i) {
         var current = $(this);
@@ -34,7 +34,7 @@ $(document).ready(function() {
         var j = prevh4.attr("id").substring(2);
         $("#linkh4" + j).next("ul").append("<li><a id='linkh5" + i + "' href='#h5" +
             i + "' title='" + current.attr("tagName") + "'>" +
-            current.html() + "</a><ul></ul></li>");
+            current.html() + '</a><ul class="nav nav-pills nav-stacked"></ul></li>');
     });
     $("h6").each(function(i) {
         var current = $(this);
@@ -43,12 +43,12 @@ $(document).ready(function() {
         var j = prevh5.attr("id").substring(2);
         $("#linkh5" + j).next("ul").append("<li><a id='linkh6" + i + "' href='#h6" +
             i + "' title='" + current.attr("tagName") + "'>" +
-            current.html() + "</a><ul></ul></li>");
+            current.html() + '</a><ul class="nav nav-pills nav-stacked"></ul></li>');
     });
 });
 
 
-$(function() {
-    var $affixElement = $('div[data-spy="affix"]');
-    $affixElement.width($affixElement.parent().width());
-});
+// $(function() {
+//     var $affixElement = $('div[data-spy="affix"]');
+//     $affixElement.width($affixElement.parent().width());
+// });
