599f989b805edfb5b390e70747930c3a3734ce60
iadfeshchm <zlatov@ya.ru>
2016-09-16 11:26:05 +0300

    q

---
+++ php/env/test.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+echo "<pre>";
+print_r($_ENV['REMOTE_ADDR']);
+echo "</pre>";
+
+echo "<pre>";
+print_r(getenv("REMOTE_ADDR"));
+echo "</pre>";
+
+echo "<pre>";
+print_r($_ENV['REMOTE_ADDR']);
+echo "</pre>";
+
+
+
+
+// phpinfo();
+
+echo "<pre>";
+print_r($_ENV);
+echo "</pre>";
--- tree/ct/create.php
+++ tree/ct/create.php
@@ -1,5 +1,11 @@
 <?php
 // CREATE SCHEMA `tree` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+// 
+// CREATE TABLE `tree`.`ct_tree` (
+//   `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+//   `header` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' NOT NULL,
+//   PRIMARY KEY (`id`));
+
 include_once 'config.php';
 try {
 
