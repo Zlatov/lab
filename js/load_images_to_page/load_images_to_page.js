6a32adf3d2280a00da7d2914b29cda8ec93d8932
iadfeshchm <zlatov@ya.ru>
2017-03-28 09:40:44 +0300

    q

--- js/load_images_to_page/load_images_to_page.js
+++ js/load_images_to_page/load_images_to_page.js
@@ -1,52 +1,64 @@
 $(document).ready(()=>{
-  function preview_images(event) {
-    var files = this.files;
-    var preview_div     = $('#js_preview_div');
-    readers = []
+
+  // Обработчик изменения содержимого input type file
+  var preview_images = function() {
+    var files = this.files
+    var div = $('#js_preview_div')
+    div.html('')
     if (files.length) {
-      for (var i = 0; i < files.length; i++) {
-        var file = files[i]
-        var reader  = new FileReader()
-        var img = $("<img>")
-
-        img[0].onload = function(){
-          var img = $(this);
-          var minImgWidht = 250;
-          var minImgHeight = 300;
-          var maxPrevieWidth = 600;
-          var maxPrevieHeight = 300;
-          var maxk = maxPrevieWidth/maxPrevieHeight;
-          var imgWidth = this.naturalWidth;
-          var imgHeight = this.naturalHeight;
-          var imgk = imgWidth/imgHeight;
-          if (imgk > maxk) {
-            // ограничивать по ширине
-            var previeWidth = maxPrevieWidth;
-            var k = previeWidth/imgWidth;
-            var previeHeight = Math.floor(k*imgHeight);
-          } else {
-            // ограничивать по высоте
-            var previeHeight = maxPrevieHeight;
-            var k = previeHeight/imgHeight;
-            var previeWidth = Math.floor(k*imgWidth);
+      // Один реадер для чтения всех файлов,
+      // множество реадеров - не дает множественную обработку загрузки
+      // (загружает только первое изображение на относительно слабых компах)
+      var reader  = new FileReader()
+      // счётчик обработанных файлов
+      reader.fileindex = 0
+      // рекурсивная функция
+      function readFiles() {
+        if (files.length > reader.fileindex) {
+          var img = $("<img>")
+          var file = files[reader.fileindex]
+          reader.fileindex = reader.fileindex + 1
+          reader.onloadend = function(loadEvent) {
+            // что происходит когда в реадер поступила инфа
+            img.appendTo(div)
+            img[0].onload = function(){
+              var img = $(this);
+              var maxPrevieWidth = 100;
+              var maxPrevieHeight = 100;
+              var maxk = maxPrevieWidth/maxPrevieHeight
+              var imgWidth = this.naturalWidth
+              var imgHeight = this.naturalHeight
+              var imgk = imgWidth/imgHeight
+              if (imgk > maxk) {
+                // ограничивать по ширине
+                var previeWidth = maxPrevieWidth
+                var k = previeWidth/imgWidth
+                var previeHeight = Math.floor(k*imgHeight)
+              } else {
+                // ограничивать по высоте
+                var previeHeight = maxPrevieHeight
+                var k = previeHeight/imgHeight
+                var previeWidth = Math.floor(k*imgWidth)
+              }
+              img.width(previeWidth)
+              img.height(previeHeight)
+            }
+            img[0].src=reader.result
+            readFiles()
           }
-          var minAreaWidth = Math.ceil(k*minImgWidht);
-          var minAreaHeight = Math.ceil(k*minImgHeight);
-          img.width(previeWidth);
-          img.height(previeHeight);
+          reader.readAsDataURL(file)
+        } else {
+          // finishedReadingFiles() // no more files to read
         }
-        
-        reader.onloadend = function(){
-          img.appendTo(preview_div)
-          img[0].src=reader.result
-        }
-
-        reader.readAsDataURL(file)
       }
+      readFiles()
+      div.slideDown()
     } else {
-      preview_div.html('')
-      preview_div.slideUp();
+      div.html('')
+      div.slideUp()
     }
   }
-  $('#js_form').on('change', '#js_input_images', preview_images);
+
+  $('#js_form').on('change', '#js_input_images', preview_images)
+
 })
