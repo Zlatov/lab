7b475fbef1404701bb6921792fd9f352c231ba6b
iadfeshchm <zlatov@ya.ru>
2017-06-19 16:57:22 +0300

    q

---
+++ js/hash/hash.js
@@ -0,0 +1,29 @@
+console.log('Доступ к данным')
+console.log('a = {a:1}: ', a = {a:1})
+console.log('a.a: ', a.a)
+console.log('a["a"]: ', a["a"])
+console.log('a.z: ', a.z)
+console.log('a["z"]: ', a["z"])
+
+console.log('Слияние')
+console.log('a = {a:1}: ', a = {a:1})
+console.log('b = {b:2}: ', b = {b:2})
+console.log('c = {}: ', c = {})
+console.log('Object.assign({},a,b): ', Object.assign({},a,b))
+console.log('a: ', a)
+console.log('Object.assign({},a,b,c): ', Object.assign({},a,b,c))
+
+console.log('Клонирование (то же что слияние с {})')
+console.log('a = {a:1}: ', a = {a:1})
+console.log('b = Object.assign({},a): ', b = Object.assign({},a))
+console.log('a.a=2: ', a.a=2)
+console.log('a.a: ', a.a)
+console.log('b.a: ', b.a)
+
+console.log('Перебор')
+console.log('for (var key in b) {...')
+for (var key in b) {
+  console.log('key: ', key)
+  console.log('b[key]: ', b[key])
+}
+
--- js/string/string.js
+++ js/string/string.js
@@ -1,3 +1,8 @@
 var str = "How are you doing today?";
 var res = str.split(" ");
 console.log('res: ', res)
+
+console.log( '.A.'.replace(/A/g, '!'))
+
+console.log('substring')
+console.log('\'Цвет кармана\'.substring(0,4): ', 'Цвет кармана'.substring(0,4))
