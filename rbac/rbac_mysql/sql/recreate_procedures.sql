61232edea437dfd9893f7d4c479202d8dbcfafd2
iadfeshchm <zlatov@ya.ru>
2017-09-08 09:47:10 +0300

    rbac связывание графа

--- rbac/rbac_mysql/sql/recreate_procedures.sql
+++ rbac/rbac_mysql/sql/recreate_procedures.sql
@@ -5,8 +5,8 @@ DROP PROCEDURE IF EXISTS `add_object`; -- Добавить объект
 DROP PROCEDURE IF EXISTS `assign_role_parent`; -- Назначить роли предка (другую роль)
 DROP PROCEDURE IF EXISTS `assign_permission_role`; -- Дать разрешение роли
 DROP PROCEDURE IF EXISTS `assign_role_user`; -- Назначить роль пользователя
-DROP PROCEDURE IF EXISTS `get_role_immediate_perm_on_object`;
-DROP PROCEDURE IF EXISTS `get_role_perm`;
+DROP PROCEDURE IF EXISTS `get_role_immediate_perm_on_object`; -- Получить права назначенные непосредственно роли
+DROP PROCEDURE IF EXISTS `get_role_perm`; -- Получить права роли
 DROP PROCEDURE IF EXISTS `get_roles`;
 DROP PROCEDURE IF EXISTS `get_roles_rel`;
 DROP PROCEDURE IF EXISTS `get_users`;
@@ -21,7 +21,7 @@ procedure_label:BEGIN
 	-- mysql> SELECT CHAR_LENGTH('重庆'); -> 2 (characters)
 	SELECT CHAR_LENGTH( param_name ) INTO nameCharLength;
 	IF (nameCharLength > 128) THEN
-		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Длинна имени пользователя превышает допустимое значение.';
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Длинна имени пользователя превышает допустимое значение.';
 		LEAVE procedure_label;
 	END IF;
 	INSERT INTO `user` (`name`) VALUES (param_name);
@@ -50,14 +50,34 @@ END;;
 
 CREATE PROCEDURE `assign_role_parent`(IN param_did INT UNSIGNED, IN param_aid INT UNSIGNED)
 procedure_label:BEGIN
+	IF param_did <=> param_aid OR param_did <=> NULL OR param_aid <=> NULL THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Не верные входные данные.';
+		LEAVE procedure_label;
+	END IF;
 	INSERT INTO `rbac_rolerel` (`aid`, `did`, `dif`) VALUES (param_aid, param_did, 1);
-	-- Вставляем связи
+	-- Вставляем связи между предками нового родителя и новым ребёнком
 	INSERT INTO `rbac_rolerel` (`aid`, `did`, `dif`)
-		-- Выбираем предков указанного родителя (did = idРодителя)
-		-- и вставляем записи типа: idПредка, нашId
-		SELECT `aid`, param_did, 2
+		-- Выбираем предков нового родителя (did = param_aid)
+		-- и вставляем записи типа: найденныеПредки, новыйРебёнок
+		SELECT `aid`, param_did, dif + 1
 		FROM `rbac_rolerel`
 		WHERE `did` = param_aid;
+	-- Вставляем связи между потомками нового ребёнка и новым родителем
+	INSERT INTO `rbac_rolerel` (`aid`, `did`, `dif`)
+		-- Выбираем потомков нового ребенка (aid = param_did)
+		-- и вставляем записи типа: новыйРодитель, найденныеПотомки
+		SELECT param_aid, `did`, dif + 1
+		FROM `rbac_rolerel`
+		WHERE `aid` = param_did;
+	-- Вставляем связи между потомками нового ребёнка и предками нового родителя
+	INSERT INTO `rbac_rolerel` (`aid`, `did`, `dif`)
+		-- Выбираем потомков нового ребенка (rr1.aid = param_did),
+		-- выбираем предков нового родителя (rr2.did = param_aid)
+		-- из пересечения двух таблиц составляем связи
+		SELECT `rr2`.`aid`, `rr1`.`did`, `rr1`.`dif` + `rr2`.`dif` + 1
+		FROM `rbac_rolerel` rr1
+		JOIN `rbac_rolerel` rr2
+		WHERE `rr1`.`aid` = param_did AND `rr2`.`did` = param_aid;
 END;;
 
 CREATE PROCEDURE `get_roles`()
