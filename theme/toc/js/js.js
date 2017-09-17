3cb08d5ee654388e7763b0dcc728993f71fa1517
iadfeshchm <zlatov@ya.ru>
2017-09-17 17:11:52 +0300

    q

--- theme/toc/js/js.js
+++ theme/toc/js/js.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         var current = $(this);
         current.attr("id", "h2" + i);
         $("#toc").children("ul").append("<li><a id='linkh2" + i + "' href='#h2" +
-            i + "' title='" + current.attr("tagName") + "'>" +
+            i + "' title='" + (current.attr("title")||'') + "'>" +
             current.html() + '</a><ul class="nav nav-pills nav-stacked"></ul></li>');
     });
     $("h3").each(function(i) {
@@ -14,7 +14,7 @@ $(document).ready(function() {
         var prevh2 = current.prevAll("h2").first();
         var j = prevh2.attr("id").substring(2);
         $("#linkh2" + j).next("ul").append("<li><a id='linkh3" + i + "' href='#h3" +
-            i + "' title='" + current.attr("tagName") + "'>" +
+            i + "' title='" + (current.attr("title")||'') + "'>" +
             current.html() + '</a><ul class="nav nav-pills nav-stacked"></ul></li>');
     });
     $("h4").each(function(i) {
@@ -24,7 +24,7 @@ $(document).ready(function() {
         var j = prevh3.attr("id").substring(2);
         alert
         $("#linkh3" + j).next("ul").append("<li><a id='linkh4" + i + "' href='#h4" +
-            i + "' title='" + current.attr("tagName") + "'>" +
+            i + "' title='" + (current.attr("title")||'') + "'>" +
             current.html() + '</a><ul class="nav nav-pills nav-stacked"></ul></li>');
     });
     $("h5").each(function(i) {
@@ -33,7 +33,7 @@ $(document).ready(function() {
         var prevh4 = current.prevAll("h4").first();
         var j = prevh4.attr("id").substring(2);
         $("#linkh4" + j).next("ul").append("<li><a id='linkh5" + i + "' href='#h5" +
-            i + "' title='" + current.attr("tagName") + "'>" +
+            i + "' title='" + (current.attr("title")||'') + "'>" +
             current.html() + '</a><ul class="nav nav-pills nav-stacked"></ul></li>');
     });
     $("h6").each(function(i) {
@@ -42,7 +42,7 @@ $(document).ready(function() {
         var prevh5 = current.prevAll("h5").first();
         var j = prevh5.attr("id").substring(2);
         $("#linkh5" + j).next("ul").append("<li><a id='linkh6" + i + "' href='#h6" +
-            i + "' title='" + current.attr("tagName") + "'>" +
+            i + "' title='" + (current.attr("title")||'') + "'>" +
             current.html() + '</a><ul class="nav nav-pills nav-stacked"></ul></li>');
     });
 });
