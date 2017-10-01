b67ef350d0d71794132b4546d321848dda949549
iadfeshchm <zlatov@ya.ru>
2017-10-01 15:59:31 +0300

    q

--- rbac/rbac_mysql/config-example.php
+++ rbac/rbac_mysql/config-example.php
@@ -1,8 +1,13 @@
 <?php
 $config = [
-	'user' => 'root',
-	'password' => 'password',
-	'host' => 'localhost',
-	'dbName' => 'lab',
-	'charset' => 'utf8',
+	'db' => [
+		'user' => 'root',
+		'password' => 'password',
+		'host' => 'localhost',
+		'dbName' => 'lab',
+		'charset' => 'utf8'
+	],
+	'user' => [
+		'limit' => 10
+	]
 ];
