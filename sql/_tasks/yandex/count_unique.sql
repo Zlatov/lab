3654745dc1ac62f264f7246b8f19f591ece8748a
iadfeshchm <zlatov@ya.ru>
2017-11-03 15:37:27 +0300

    q

--- sql/_tasks/yandex/count_unique.sql
+++ sql/_tasks/yandex/count_unique.sql
@@ -5,16 +5,45 @@ CREATE TABLE IF NOT EXISTS `count_unique` (
 );
 
 INSERT INTO `count_unique` VALUES
-(1, 'a'),
-(2, 'c'),
-(3, 'b'),
-(4, 'b'),
-(5, 'a'),
-(6, 'b'),
-(7, 'c');
+(1, 'яблоки'),
+(2, 'бананы'),
+(3, 'груши'),
+(4, 'груши'),
+(5, 'яблоки'),
+(6, 'груши'),
+(7, 'что-то чего не должно быть'),
+(8, 'бананы');
 
-SELECT * FROM `count_unique`;
+-- SELECT * FROM `count_unique`;
 
 SELECT `string`, count(`numbers`)
 FROM `count_unique`
-GROUP BY `string`
+GROUP BY `string`;
+
+-- Неправильный подход к программированию в SQL:
+-- SELECT
+-- 	b.string
+-- 	,b.numbers
+-- 	,c.numbers
+-- 	-- ,d.numbers
+-- FROM (
+-- 	SELECT
+-- 		distinct a.string,
+-- 		a.numbers,
+-- 		count(a.numbers) as count
+-- 	FROM count_unique a
+-- 	GROUP BY a.string
+-- 	HAVING count > 1
+-- ) as b
+-- LEFT JOIN count_unique c ON c.string = b.string AND c.numbers <> b.numbers
+-- LEFT JOIN count_unique d ON d.string = c.string AND d.numbers <> c.numbers
+
+SELECT
+	a.string,
+	a.numbers,
+	b.numbers
+FROM count_unique a, count_unique b
+WHERE
+	a.numbers <> b.numbers
+	AND a.string = b.string
+GROUP BY a.string, a.numbers + b.numbers
