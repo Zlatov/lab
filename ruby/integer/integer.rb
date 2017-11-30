f3b321d1b9168acfc2045692ef6c243c600cb4c7
iadfeshchm <zlatov@ya.ru>
2017-11-30 09:18:28 +0300

    q

--- ruby/integer/integer.rb
+++ ruby/integer/integer.rb
@@ -30,3 +30,17 @@ puts a % 100.01
 puts a % 100.1
 
 puts "#{'%g' % 5.0000}"
+puts ("%.2f" % 12000000.549)
+puts ("%.2f" % 12000000.00)
+puts ("%.2f" % 12000000)
+
+for price in [99, 99.00, 99.99, 99.888, 99.999, 9999999, 9999999.00, 9999999.99, 9999999.888, 9999999.999]
+  price_with_pennies = "%.2f" % price
+  pennies = price_with_pennies.slice(-2, 2)
+  formated_price = if pennies == '00'
+    price_with_pennies.slice 0, price_with_pennies.length-3
+  else
+    price_with_pennies
+  end
+  print 'formated_price: '.red; puts formated_price
+end
