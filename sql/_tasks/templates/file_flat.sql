482a3fbf47b02d314c15c63571df2a3954331c14
iadfeshchm <zlatov@ya.ru>
2017-12-05 09:47:45 +0300

    q

--- sql/_tasks/templates/file_flat.sql
+++ sql/_tasks/templates/file_flat.sql
@@ -16,7 +16,7 @@ CREATE TABLE `file` (
   `keywords` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
 
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uq_sid` (`sid`)
+  UNIQUE KEY `uq_file_sid` (`sid`)
 )
 ENGINE=InnoDB
 AUTO_INCREMENT 1
@@ -28,7 +28,4 @@ INSERT INTO `file` (`sid`, `header`)
 VALUES ('1', '1'),
 ('2', '2');
 
-SELECT
-	*
-FROM
-	`file`
+SELECT * FROM `file`;
