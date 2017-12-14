857d090b08996b0579060763fa4f4ec401616ee3
iadfeshchm <zlatov@ya.ru>
2017-12-14 09:32:09 +0300

    q

--- sql/_tasks/templates/file_flat.sql
+++ sql/_tasks/templates/file_flat.sql
@@ -1,11 +1,13 @@
 -- Удаляем таблицу если есть.
 DROP TABLE IF EXISTS `file`;
 
+
 -- Создаём таблицу file
 CREATE TABLE `file` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `sid` VARCHAR(160) NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  `sid` VARCHAR(160) NOT NULL,
   `header` VARCHAR(255) NOT NULL,
 
   `updated_at` TIMESTAMP NULL,
@@ -24,8 +26,19 @@ DEFAULT CHARSET=utf8
 COLLATE utf8_general_ci
 COMMENT 'Файлы';
 
-INSERT INTO `file` (`sid`, `header`)
-VALUES ('1', '1'),
-('2', '2');
+
+INSERT INTO `file`
+  (`sid`, `header`)
+VALUES
+  ('1', '1'),
+  ('2', '2');
+
+DELETE 
+FROM `file`
+WHERE `sid` = '1';
+
+UPDATE `file`
+SET `sid` = '3', `header` = '3'
+WHERE id = 2;
 
 SELECT * FROM `file`;
