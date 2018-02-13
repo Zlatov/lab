f6d333e5748e92a2830197fb1eb1bacb1fb94607
iadfeshchm <zlatov@ya.ru>
2018-02-13 13:23:42 +0300

    q

--- js/_tasks/market_slider/liner.js
+++ js/_tasks/market_slider/liner.js
@@ -4,61 +4,122 @@
   window.Market = {}
 
   // Типа слайдер,
-  // прокручиваем линейку не важно с чем (не со слайдами), на фиксированное расстояние.
+  // прокручиваем линейку не важно с чем (не со слайдами например), на фиксированное расстояние.
   window.Market.Liner = function(place, options=null) {
     if (!(place instanceof window.jQuery) || !place.length) {
       throw new window.Error('place is not jQuery object.')
     }
-    this.place = place
     this.options = Object.assign(window.Market.Liner.options, options||{})
+    this.place = place
+    this.liner_id = place.data('liner_id')
+    this.line = place.children(this.options.line_selector)
+    this.next_selector = this.options.next_selector + '[data-liner_id="' + this.liner_id + '"]'
+    this.prev_selector = this.options.prev_selector + '[data-liner_id="' + this.liner_id + '"]'
+    this.next = $(this.next_selector)
+    this.prev = $(this.prev_selector)
     this.position = 0
-    this.count = this.place.children(this.options.division_selector).length
+    this.count = this.line.children(this.options.division_selector).length
+    return this
   }
+
+  window.Market.Liner.prototype.activate = function() {
+    this.activate_ui()
+    return this
+  }
+
+  window.Market.Liner.prototype.activate_ui = function() {
+    this.activate_scroll()
+    this.ui_buttons_apply_position()
+  }
+
   window.Market.Liner.prototype.activate_scroll = function() {
     var listener = $(this.options.listener_selector)
-    listener.on('click', this.options.button_next_selector, {instance: this}, this.next_handler)
-    listener.on('click', this.options.button_prev_selector, {instance: this}, this.prev_handler)
+    listener.on('click', this.next_selector, {instance: this}, this.next_handler)
+    listener.on('click', this.prev_selector, {instance: this}, this.prev_handler)
   }
+
   window.Market.Liner.prototype.next_handler = function(event) {
     var instance = event.data.instance
     var new_position = instance.calc_position(1)
-    instance.set_position(new_position)
+    if (new_position != instance.position) {
+      instance.set_position(new_position)
+      instance.ui_apply_position()
+    }
   }
+
   window.Market.Liner.prototype.prev_handler = function(event) {
     var instance = event.data.instance
     var new_position = instance.calc_position(-1)
-    instance.set_position(new_position)
+    if (new_position != instance.position) {
+      instance.set_position(new_position)
+      instance.ui_apply_position()
+    }
   }
+
   window.Market.Liner.prototype.calc_left = function() {
     return this.position * -this.options.division_width
   }
+
   window.Market.Liner.prototype.calc_position = function(direction=1) {
     var new_position = this.position + direction * this.options.division_step
-    if (new_position + 1 > this.count - this.options.window_division) {
-      new_position = this.count - this.options.window_division
-    }
-    if (new_position < 0) {
-      new_position = 0
-    }
+    new_position = this.correct_position(new_position)
     return new_position
   }
+  window.Market.Liner.prototype.correct_position = function(position) {
+    if (position + 1 > this.count - this.options.window_division) {
+      position = this.count - this.options.window_division
+    }
+    if (position < 0) {
+      position = 0
+    }
+    return position
+  }
+
   window.Market.Liner.prototype.set_position = function(position) {
+    position = this.correct_position(position)
     this.position = position
-    this.ui_apply_position()
   }
+
   window.Market.Liner.prototype.ui_apply_position = function() {
-    var left = this.calc_left()
-    this.place.css('left', left + 'px')
+    this.ui_line_apply_position()
+    this.ui_buttons_apply_position()
   }
+
+  window.Market.Liner.prototype.ui_line_apply_position = function() {
+    var left = this.calc_left()
+    this.line.css('left', left + 'px')
+  }
+
+  window.Market.Liner.prototype.ui_buttons_apply_position = function() {
+    this.ui_prev_apply_position()
+    this.ui_next_apply_position()
+  }
+
+  window.Market.Liner.prototype.ui_prev_apply_position = function() {
+    if (this.position == 0) {
+      this.prev.addClass('disable')
+    } else {
+      this.prev.removeClass('disable')
+    }
+  }
+
+  window.Market.Liner.prototype.ui_next_apply_position = function() {
+    if (this.position >= this.count - this.options.window_division) {
+      this.next.addClass('disable')
+    } else {
+      this.next.removeClass('disable')
+    }
+  }
+
   window.Market.Liner.options = {
-    place_selector: '.cat_products',
     listener_selector: 'body',
-    button_next_selector: '.products .pre_slider-next',
-    button_prev_selector: '.products .pre_slider-prev',
+    next_selector: '.liner-next',
+    prev_selector: '.liner-prev',
+    line_selector: '.liner-line',
     division_selector: '.product',
     division_width: 218,
     window_division: 4,
-    division_step: 2
+    division_step: 1
   }
 
 })();
--- js/_tasks/market_slider/market_slider.js
+++ js/_tasks/market_slider/market_slider.js
@@ -1,22 +1,7 @@
 $(document).ready(function() {
 
-  window.liner = new window.Market.Liner($('.products .cat_products .line'))
-  console.log('window.liner: ', window.liner)
-  window.liner.activate_scroll()
-
-  /*
-  var next = $('.pre_slider-next')
-  next.on('click', function(event) {
-    var button = $(this)
-    var cat_products = button.parent().next('.cat_products')
-    console.log('cat_products: ', cat_products)
-    var line = cat_products.children('.line')
-    var left = parseInt(line.css('left'))
-    console.log('left: ', left)
-    line.css('left', (left - 214) + 'px')
-  })
-  */
-
+  window.liner = new window.Market.Liner($('.products .liner')).activate()
+  
   var button_show_all_products = $('.js_show_all_products')
   button_show_all_products.on('click', function(event) {
     var products = $('.products')
