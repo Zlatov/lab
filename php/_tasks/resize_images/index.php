77798eacac45dea47639973b3a034f5b5e79ae3a
iadfeshchm <zlatov@ya.ru>
2018-04-19 22:51:15 +0300

    q

--- php/_tasks/resize_images/index.php
+++ php/_tasks/resize_images/index.php
@@ -1,9 +1,21 @@
 <?php
 
 require_once "../../../vendor/autoload.php";
+// require_once "../../../vendor/zlatov/resize-img/src/ResizeImg.php";
 
-use zlatov\tree\Tree;
+if (class_exists('Zlatov\ResizeImg')) {
+  print('exist.' . PHP_EOL);
+} else {
+  print('not exist.' . PHP_EOL);
+}
+
 use zlatov\ResizeImg;
+
+if (class_exists('Zlatov\ResizeImg')) {
+  print('exist.' . PHP_EOL);
+} else {
+  print('not exist.' . PHP_EOL);
+}
 
 print_r(get_declared_classes());
 print PHP_EOL;
