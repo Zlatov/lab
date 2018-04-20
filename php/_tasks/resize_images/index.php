85c3e4103e05c05ddb4e0c8c6167d442d70fcc64
iadfeshchm <zlatov@ya.ru>
2018-04-20 16:41:06 +0300

    q

--- php/_tasks/resize_images/index.php
+++ php/_tasks/resize_images/index.php
@@ -1,23 +1,68 @@
 <?php
 
 require_once "../../../vendor/autoload.php";
-// require_once "../../../vendor/zlatov/resize-img/src/ResizeImg.php";
-
-if (class_exists('Zlatov\ResizeImg')) {
-  print('exist.' . PHP_EOL);
-} else {
-  print('not exist.' . PHP_EOL);
-}
 
 use zlatov\ResizeImg;
 
-if (class_exists('Zlatov\ResizeImg')) {
-  print('exist.' . PHP_EOL);
-} else {
-  print('not exist.' . PHP_EOL);
+// $a = 'Zlatov\ResizeImg\ResizeImg_Errors'; print(class_exists($a) ? "+: $a" : "-: $a");
+
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/views');
+$twig = new Twig_Environment($loader, array(
+  'cache' => __DIR__ . '/views/cache',
+  'auto_reload' => true
+));
+
+if (isset($_POST)&&!empty($_POST)) {
+  $path = __DIR__;
+  $dir = opendir($path);
+  chdir($path);
+
+  $paramImg = [
+    'width' => (int)$_POST['imgwidth'],
+    'height' => (int)$_POST['imgheight'],
+    'kind' => $_POST['imgtype'],
+    'subFolder' => (isset($_POST['subfolder'])&&($_POST['subfolder']==='true'))?true:false,
+    'nameSufix' => false,
+  ];
+  $paramThumbnail = [
+    'width' => (int)$_POST['thumbnailwidth'],
+    'height' => (int)$_POST['thumbnailheight'],
+    'kind' => $_POST['thumbnailtype'],
+    'subFolder' => (isset($_POST['subfolder'])&&($_POST['subfolder']==='true'))?true:false,
+    'nameSufix' => true,
+  ];
+
+  while ($f = readdir($dir))
+  {
+    if (is_file($f) && ($f !== ".") && ($f !== "..") && in_array(mb_strtolower(pathinfo($f)['extension']), ['png','jpg','jpeg']))
+    {
+      $pathToFile = $path . DIRECTORY_SEPARATOR . $f;
+      try {
+        $img = new ResizeImg($pathToFile);
+        switch ($_POST['do']) {
+          case 'resizeandthumbnails':
+            $img->resize($paramImg);
+            echo '<p>' . $img->imgTag . '</p>';
+            $img->resize($paramThumbnail);
+            echo '<p>' . $img->imgTag . '</p>';
+            break;
+          case 'thumbnails':
+            $img->resize($paramThumbnail);
+            echo '<p>' . $img->imgTag . '</p>';
+            break;
+          case 'resize':
+            $img->resize($paramImg);
+            echo '<p>' . $img->imgTag . '</p>';
+            break;
+        }
+        $img = null;
+      } catch (Exception $e) {
+        echo "<p>Ошибка: " . $e->getMessage() . "</p>";
+      }
+    }
+  }
 }
 
-print_r(get_declared_classes());
-print PHP_EOL;
-
-// class_exists('MyClass')
+echo $twig->render('index/index.html', []);
