3847870f3286c91bb27925972cef55d47d481758
iadfeshchm <zlatov@ya.ru>
2018-08-21 11:08:17 +0300

    q

---
+++ git/submodules/submodules.bash
@@ -0,0 +1,45 @@
+exit 0
+
+
+# Добавление субмодуля
+# В корне проекта к которому добавляем выполнить:
+git submodule add git@github.com:Zlatov/Zlader.git js/_libs/zlader
+# появятся новые файлы: .gitmodules, submodule_name
+# закомитить...
+
+
+# Клонирование проекта с субмодулями
+git clone http:... ./
+git submodule init
+git submodule update
+# Или
+git clone --recursive https:...
+
+
+# Обновление отдельного субмодуля
+# перейти в его директорию и проверить наличие изменений:
+cd js/_libs/zlader
+git fetch
+# И затем для обновления выполнить:
+git merge
+
+
+# Удаление подмодуля
+# 1. Удалить нужную секцию в файле .gitmodules. 
+subl .gitmodules
+# [submodule "submodule_name"]
+# path = submodule_name 
+# url = git://github.com/any_user_name/submodule_name.git
+# 2. Зафиксировать изменения, выполнив:
+git add .gitmodules
+# 3. Удалить соответствующие модулю строки из .git/config.
+subl .git/config
+# [submodule "submodule_name"] 
+# url = git://github.com/any_user_name/submodule_name.git
+# 4. Удалить пути, созданные для подмодулей, из области индексирования:
+git rm --cached PATH/TO/SUBMODULE
+# 5. Удалить каталог подмодуля:
+rm -rf .git/modules/submodule_name
+# 6. Сделать коммит изменений.
+# 7. Удалить ненужные файлы подмодулей:
+rm -rf path/to/submodule_name
