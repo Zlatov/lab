561f3af7c66391f374d2a094617a8e9279e9686b
iadfeshchm <zlatov@ya.ru>
2018-08-22 21:02:23 +0300

    q

--- linux/system_setup/system_setup.sh
+++ linux/system_setup/system_setup.sh
@@ -3,3 +3,8 @@
 # 
 # cd ~ && mkdir -p projects/my/lab && git clone git@github.com:Zlatov/lab.git projects/my/lab && projects/my/lab/linux/system_setup/system_setup.sh
 # 
+
+lab_path=projects/my/lab
+. lab_path/bash/_lib/echoc
+
+echoc 'Лаборатория склонирована' green
