253b1fc6a9ebf90f81e0b1f4aa4bb74175bd9dd8
iadfeshchm <zlatov@ya.ru>
2018-08-22 21:09:50 +0300

    q

--- linux/system_setup/system_setup.sh
+++ linux/system_setup/system_setup.sh
@@ -7,4 +7,12 @@
 lab_path=projects/my/lab
 . lab_path/bash/_lib/echoc
 
-echoc 'Лаборатория склонирована' green
+echoc 'Лаборатория склонирована.' green
+
+echoc 'Установка yarn.' yellow
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
+echoc 'Yarn установлен.' yellow
