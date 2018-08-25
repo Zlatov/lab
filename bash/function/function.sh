7341ec1510925e6b994652a1abaab1888e691100
iadfeshchm <zlatov@ya.ru>
2018-08-25 21:25:58 +0300

    q

--- bash/function/function.sh
+++ bash/function/function.sh
@@ -54,7 +54,7 @@ function coffee {
   echo DEFAULT = "${DEFAULT}"
 
   echoc "Имя функции: ${FUNCNAME}" blue
-  echoc "Количество переданных параметров : $#" green
+  echoc "Количество переданных параметров: $#" green
   echo "Все параметры переданные функции: '$@'"
 }
 
@@ -67,3 +67,91 @@ function coffee {
 # coffee -d 3 -s 10 
 # coffee -d -s 10
 # Опции превращаются в параметры
+
+
+
+
+
+
+
+# Ещё 2 способа:
+
+# Bash Equals-Separated (e.g., --option=argument) (without getopt[s])
+# Usage ./myscript.sh -e=conf -s=/etc -l=/usr/lib /etc/hosts
+
+# for i in "$@"
+# do
+# case $i in
+#     -e=*|--extension=*)
+#     EXTENSION="${i#*=}"
+#     shift # past argument=value
+#     ;;
+#     -s=*|--searchpath=*)
+#     SEARCHPATH="${i#*=}"
+#     shift # past argument=value
+#     ;;
+#     -l=*|--lib=*)
+#     LIBPATH="${i#*=}"
+#     shift # past argument=value
+#     ;;
+#     --default)
+#     DEFAULT=YES
+#     shift # past argument with no value
+#     ;;
+#     *)
+#           # unknown option
+#     ;;
+# esac
+# done
+# echo "FILE EXTENSION  = ${EXTENSION}"
+# echo "SEARCH PATH     = ${SEARCHPATH}"
+# echo "LIBRARY PATH    = ${LIBPATH}"
+# echo "Number files in SEARCH PATH with EXTENSION:" $(ls -1 "${SEARCHPATH}"/*."${EXTENSION}" | wc -l)
+# if [[ -n $1 ]]; then
+#     echo "Last line of file specified as non-opt/last argument:"
+#     tail -1 $1
+# fi
+
+# To better understand ${i#*=} search for "Substring Removal" in this guide. It is functionally equivalent to `sed 's/[^=]*=//' <<< "$i"` which calls a needless subprocess or `echo "$i" | sed 's/[^=]*=//'` which calls two needless subprocesses.
+
+# Method #2: Using bash with getopt[s]
+# from: http://mywiki.wooledge.org/BashFAQ/035#getopts
+# getopt(1) limitations (older, relatively-recent getopt versions):
+# can't handle arguments that are empty strings
+# can't handle arguments with embedded whitespace
+# More recent getopt versions don't have these limitations.
+# Additionally, the POSIX shell (and others) offer getopts which doesn't have these limitations. Here is a simplistic getopts example:
+
+# # A POSIX variable
+# OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# # Initialize our own variables:
+# output_file=""
+# verbose=0
+
+# while getopts "h?vf:" opt; do
+#     case "$opt" in
+#     h|\?)
+#         show_help
+#         exit 0
+#         ;;
+#     v)  verbose=1
+#         ;;
+#     f)  output_file=$OPTARG
+#         ;;
+#     esac
+# done
+
+# shift $((OPTIND-1))
+
+# [ "${1:-}" = "--" ] && shift
+
+# echo "verbose=$verbose, output_file='$output_file', Leftovers: $@"
+
+# # End of file
+
+# The advantages of getopts are:
+# It's more portable, and will work in other shells like dash.
+# It can handle multiple single options like -vf filename in the typical Unix way, automatically.
+# The disadvantage of getopts is that it can only handle short options (-h, not --help) without additional code.
+# There is a getopts tutorial which explains what all of the syntax and variables mean. In bash, there is also help getopts, which might be informative.
--- linux/system_setup/system_setup.sh
+++ linux/system_setup/system_setup.sh
@@ -3,25 +3,56 @@
 # 
 # cd ~ && mkdir -p projects/my/lab
 # git clone git@github.com:Zlatov/lab.git projects/my/lab
-# sudo projects/my/lab/linux/system_setup/system_setup.sh
+# cd ~ && sudo projects/my/lab/linux/system_setup/system_setup.sh
 # 
 
 set -eu
 
 cd ~
 
+sudo apt-get update
+
 lab_path=projects/my/lab
 . $lab_path/bash/_lib/echoc
 
+if hash curl 2>/dev/null
+then
+  echoc 'Уже установлен curl.' blue
+else
+  echoc 'Установка curl.' yellow
+  sudo apt-get install curl
+  echoc 'Установлен curl.' green
+fi
+
 if hash yarn 2>/dev/null
 then
-  echoc 'yarn уже установлен.' blue
+  echoc 'Уже установлен yarn.' blue
 else
   echoc 'Установка yarn.' yellow
   curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
   sudo apt-get update && sudo apt-get install yarn
-  echoc 'Yarn установлен.' green
+  echoc 'Установлен yarn.' green
 fi
 
+echoc 'Настройка git gitexcludes.' yellow
 git config --global core.excludesfile ~/.gitexcludes
+cp $lab_path/linux/system_setup/system_setup/git/.gitexcludes ~/.gitexcludes
+echoc 'Настроен git gitexcludes.' green
+
+echoc 'Настройка apache2 подключаем модуль mod_rewrite.' yellow
+sudo ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite.load
+echoc 'Настроен apache2 подключён модуль mod_rewrite.' green
+
+echoc 'Настройка apache default encoding.' yellow
+echoc "Добавте директиву AddDefaultCharset UTF-8 в файл настройки." yellow
+if [ -f '/etc/apache2/conf-available/charset.conf' ]
+then
+  echoc "Файл настройки найден." green
+  subl /etc/apache2/conf-available/charset.conf
+  echo -n 'Нажмите Enter для продолжения.'; read A
+else
+  echoc 'Файл настройки не найден.' red
+  echoc "Добавте директиву AddDefaultCharset UTF-8 в файл настройки" yellow
+  echo -n 'Нажмите Enter для продолжения.'; read A
+fi
