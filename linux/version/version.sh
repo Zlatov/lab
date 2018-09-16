53eb8564f6ea7fd82b8cc2c79cf5f83d02345bc5
iadfeshchm <zlatov@ya.ru>
2018-09-16 20:13:40 +0300

    q

--- linux/version/version.sh
+++ linux/version/version.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 uname
 cat /proc/version
+
+# Ubuntu
+lsb_release -a
+# 
+# или
+# 
+# Open “System Settings” from the desktop main menu in Unity.
+# Click on the “Details” icon under “System.”
+# See version information.
