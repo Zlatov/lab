a218f9656a9a71395d145852f01ef2198c4555b3
iadfeshchm <zlatov@ya.ru>
2018-09-18 13:34:01 +0300

    q

--- php/dateAndTime/DateTime.php
+++ php/dateAndTime/DateTime.php
@@ -1,20 +1,13 @@
 <?php
 
 $dt1 = new DateTime();
-
-echo "<pre>";
 print_r($dt1);
-echo "</pre>";
 
-echo "<pre>";
 print_r($a = $dt1->getTimestamp().PHP_EOL);
 print_r($dt1->format('Y m d H i s'));
-echo "</pre>";
 
-echo "<pre>";
 print_r($dt1->setTimestamp($a+0));
 print_r(PHP_EOL.$dt1->format('Y m d H i s'));
-echo "</pre>";
 
 ////////////////
 
@@ -23,17 +16,11 @@ $dt2->setTimestamp(time() + 6666);
 
 $diff = $dt1->diff($dt2);
 
-echo "<pre>";
 print_r($diff);
-echo "</pre>";
 
-echo "<pre>";
 print_r($diff->format('%s'));
-echo "<pre>";
 
-echo "</pre>";
 print_r($dt2->getTimestamp() - $dt1->getTimestamp());
-echo "</pre>";
 
 
 
---
+++ php/date_time/date_time.php
@@ -0,0 +1,25 @@
+<?php
+
+// Секунды [Int] в дату [DateTime]
+echo PHP_EOL . 'Секунды [Int] в дату [DateTime]' . PHP_EOL;
+$a = 1522339254;
+$b = new DateTime();
+$b->setTimestamp($a);
+$c = $b->format('Y m d H i s');
+echo '$a: ' . print_r($a, true) . PHP_EOL;
+echo '$b: ' . print_r($b, true) . PHP_EOL;
+echo '$c: ' . print_r($c, true) . PHP_EOL;
+
+// Строку формата [Y-m-d H:i:s] в дату [DateTime]
+echo PHP_EOL . 'Строку формата [Y-m-d H:i:s] в дату [DateTime]' . PHP_EOL;
+$a = '2018-03-29 19:00:54';
+$b = new DateTime($a);
+echo '$a: ' . print_r($a, true) . PHP_EOL;
+echo '$b: ' . print_r($b, true) . PHP_EOL;
+
+// Дату в число
+echo PHP_EOL . 'Дату в число' . PHP_EOL;
+$a = new DateTime('2018-03-29 19:00:54');
+$b = $a->getTimestamp();
+echo '$a: ' . print_r($a, true) . PHP_EOL;
+echo '$b: ' . print_r($b, true) . PHP_EOL;
