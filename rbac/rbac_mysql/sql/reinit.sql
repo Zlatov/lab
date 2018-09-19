5267888ff8f3499b7a0c6cc7b11dcef2090516fa
iadfeshchm <zlatov@ya.ru>
2018-09-19 09:04:39 +0300

    q

--- rbac/rbac_mysql/sql/reinit.sql
+++ rbac/rbac_mysql/sql/reinit.sql
@@ -1,5 +1,5 @@
-source /home/iadfeshchm/projects/my/lab/lab/rbac/rbac_mysql/sql/recreate_schema.sql;
+source /home/iadfeshchm/projects/my/lab/rbac/rbac_mysql/sql/recreate_schema.sql;
 USE lab;
-source /home/iadfeshchm/projects/my/lab/lab/rbac/rbac_mysql/sql/recreate_tables.sql;
-source /home/iadfeshchm/projects/my/lab/lab/rbac/rbac_mysql/sql/recreate_procedures.sql;
-source /home/iadfeshchm/projects/my/lab/lab/rbac/rbac_mysql/sql/recreate_triggers.sql;
+source /home/iadfeshchm/projects/my/lab/rbac/rbac_mysql/sql/recreate_tables.sql;
+source /home/iadfeshchm/projects/my/lab/rbac/rbac_mysql/sql/recreate_procedures.sql;
+source /home/iadfeshchm/projects/my/lab/rbac/rbac_mysql/sql/recreate_triggers.sql;
