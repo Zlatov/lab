b2f235a0a0511e1b4dac71e930f834e964ca7840
iadfeshchm <zlatov@ya.ru>
2018-09-19 12:48:03 +0300

    q

--- mysql/table/table.sql
+++ mysql/table/table.sql
@@ -16,41 +16,44 @@ CREATE TABLE IF NOT EXISTS `lorem` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `sid` VARCHAR(255) NOT NULL COMMENT 'Строковый идентификатор.',
   `key` VARCHAR(64) NOT NULL,
-  `hash` VARCHAR(64) NOT NULL,
-  `not_unique_on_null` VARCHAR(64) NULL COMMENT 'Проверить что уникальность не влияет на NULL',
+  `tag` VARCHAR(64) NOT NULL DEFAULT 'def',
   `numbers` INT,
-  `header` VARCHAR(161) NOT NULL DEFAULT '',
+  `header` VARCHAR(161) NOT NULL,
+  `infoid` VARCHAR(64) NULL COMMENT 'Проверить что уникальность не влияет на NULL',
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `uq_lorem_sid` (`sid`),
-  INDEX `uq_lorem_hash` (`hash`) COMMENT 'не уникальное поле с индексацией',
-  UNIQUE KEY `uq_lorem_key` (`key`) COMMENT 'уникальное поле без индексации',
-  UNIQUE KEY `uq_lorem_notuniqueonnull` (`not_unique_on_null`)
+  INDEX        `ix_lorem_tag` (`tag`)       COMMENT 'индексное поле без ограничения на уникальность',
+  UNIQUE INDEX `uq_lorem_sid` (`sid`)       COMMENT 'уникальное поле с индексацией',
+  UNIQUE KEY   `uq_lorem_header` (`header`) COMMENT 'уникальное поле без индексации',
+  UNIQUE KEY   `uq_lorem_infoid` (`infoid`)
 )
 ENGINE InnoDB
 AUTO_INCREMENT 1
 DEFAULT CHARACTER SET utf8
 COLLATE utf8_general_ci;
+-- NNND: sid key header
+-- UQ: sid header infoid
+--  : sid key header
 
 SHOW tables;
 
--- Проверка не уникального поля с индексацией hash
-insert into `lorem` (`sid`, `key`, `hash`) values
-('1', '1', '3'),
-('2', '2', '3'),
-('3', '3', '4'),
-('4', '4', '4');
+-- Проверка не уникального поля с индексацией: tag
+insert into `lorem` (`sid`, `key`, `header`, `tag`) values
+('1', '1', '1', '3'),
+('2', '2', '2', '3'),
+('3', '3', '3', '4'),
+('4', '4', '4', '4');
 EXPLAIN `lorem`;
 EXPLAIN
 SELECT *
 FROM `lorem`
-WHERE `hash` = '4';
+WHERE `tag` = '4';
 
 -- Проверка на то что уникальность не влияет на NULL
 delete from lorem;
-insert into `lorem` (`sid`, `key`, `hash`, `not_unique_on_null`) values
-('1', '1', '3', NULL),
-('2', '2', '3', NULL),
-('3', '3', '4', '1'),
+insert into `lorem` (`sid`, `key`, `header`, `infoid`) values
+('1', '1', '1', NULL),
+('2', '2', '2', NULL),
+('3', '3', '3', '1'),
 ('4', '4', '4', '2'),
 ('5', '5', '5', NULL);
 
