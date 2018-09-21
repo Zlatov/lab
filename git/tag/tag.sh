0fe5d6fbf68a46b7ba8d542a71f6fbd2832c8a16
iadfeshchm <zlatov@ya.ru>
2018-09-21 14:27:04 +0300

    q

--- git/tag/tag.sh
+++ git/tag/tag.sh
@@ -1,10 +1,16 @@
 exit 0
 
+# Установить
 git tag  # — список существующих tag-ов
 git tag -m "описание" tagname  # — создать метку tag для текущего состояния (на текущей ветке) с именем tagname.
-git tag -d tagname  # — удалить метку tag с именем tagname
+
+# Отправить
 git push origin tag_name  # — To push a single tag
 git push --tags  # — push all tags (not recommended)
+
+# Получить
 git fetch --tags  # — fetch tags.
-git tag -d 12345  # — удалить тег.
+
+# Удалить
+git tag -d tagname  # — удалить метку tag с именем tagname
 git push --delete origin 12345  # — запушить удаление тега.
