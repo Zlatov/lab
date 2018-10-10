3f3366ce9b41ce4fbfeba779b100ec84f98eb952
iadfeshchm <zlatov@ya.ru>
2018-10-10 14:53:28 +0300

    q

---
+++ sass/console/console.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -eu
+exit 0
+
+# /usr/local/bin/sass "syntax.scss" "syntax.css"
+/usr/local/bin/sass --help
+--style=compressed # expanded (default), compressed
---
+++ scss/syntax/syntax.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -eu
+/usr/local/bin/sass "syntax.scss" "syntax.css"
