8eea3d1a33826a3b2f9716404c5bbcf2ff6099b3
iadfeshchm <zlatov@ya.ru>
2018-11-08 10:17:47 +0300

    ssh all sh

--- ssh/all.sh
+++ ssh/all.sh
@@ -2,5 +2,11 @@
 set -eu
 exit 0
 
+# Сгенерировать пару ключей
+ssh-keygen -t rsa
+
+# Копируем публичную часть на сервер
+ssh-copy-id -i ~/.ssh/id_rsa.pub myserv
+
 # Принудительно запрашивать пароль
 ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no user@host
