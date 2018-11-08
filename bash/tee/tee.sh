8c88e21d36ee3bf65faa7a19f84e3dc272646618
iadfeshchm <zlatov@ya.ru>
2018-11-08 17:52:47 +0300

    sudo tee to file

--- bash/tee/tee.sh
+++ bash/tee/tee.sh
@@ -17,3 +17,5 @@ echo asd | tee temp >/dev/null
 echoc "„Правильная“ дозапись в файл." green
 echo zxc | tee -a temp_append >/dev/null
 echo zxc | tee -a temp_append >/dev/null
+
+# echo "10.192.1.36 lab.zlatov www.lab.zlatov" | sudo tee -a /etc/hosts >/dev/null
