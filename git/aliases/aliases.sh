d004b576cb8e7f80c3d61c5afc5d7acfdd7c5a86
iadfeshchm <zlatov@ya.ru>
2018-11-09 11:29:31 +0300

    change git aliases

--- git/aliases/aliases.sh
+++ git/aliases/aliases.sh
@@ -4,8 +4,12 @@ set -eu
 
 exit 0
 
-subl ~/.bashrc
-echo "alias gca='git add -A && git commit'" | tee -a ~/.bashrc >/dev/null
+# subl ~/.bashrc
+echo "alias  s='git status -u'"            | tee -a ~/.bashrc >/dev/null
+echo "alias  a='git add'"                  | tee -a ~/.bashrc >/dev/null
+echo "alias aa='git add -A'"               | tee -a ~/.bashrc >/dev/null
+echo "alias  c='git commit'"               | tee -a ~/.bashrc >/dev/null
+echo "alias ca='git add -A && git commit'" | tee -a ~/.bashrc >/dev/null
 
 git config --global alias.hist 'log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
 git config --global alias.hist 'log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
