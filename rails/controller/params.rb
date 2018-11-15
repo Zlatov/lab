417f84e044e5ac8fea6ef4950c71c64defe79fbc
iadfeshchm <zlatov@ya.ru>
2018-11-15 11:31:56 +0300

    rails: permit nested action params

--- rails/controller/params.rb
+++ rails/controller/params.rb
@@ -1,3 +1,8 @@
 exit
 
 params.permit(:from, :to_free, :subject, :content)
+
+# Вложенные параметры вынуть и разрешить так:
+params
+  .require(:management_letter)
+  .permit(:name, :email, :message, :phonenumber, :file)
