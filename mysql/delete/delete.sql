f287da8716371d6de8dd122d8220628d4b6357b3
iadfeshchm <zlatov@ya.ru>
2018-12-21 14:47:39 +0300

    q

--- mysql/delete/delete.sql
+++ mysql/delete/delete.sql
@@ -12,20 +12,20 @@ COLLATE utf8_general_ci;
 
 INSERT INTO `test` (`n`) VALUES (1),(1),(2),(3),(3);
 
-SELECT 'Начальные данные' as '===============================';
+SELECT 'Начальные данные' as ' ';
 SELECT * FROM test;
 
-
--- SELECT
--- 	t2.*
--- FROM
--- 	(
--- 		SELECT t.n, count(t.n) AS count_dupl
--- 		FROM test t
--- 		GROUP BY t.n
--- 		HAVING count_dupl > 1
--- 	) AS t
--- LEFT JOIN test t2 ON t2.n = t.n;
+select "то, что будет удалено" as " ";
+SELECT
+	t2.*
+FROM
+	(
+		SELECT t.n, count(t.n) AS count_dupl
+		FROM test t
+		GROUP BY t.n
+		HAVING count_dupl > 1
+	) AS t
+LEFT JOIN test t2 ON t2.n = t.n;
 
 DELETE
 	t2.*
@@ -41,5 +41,5 @@ LEFT JOIN test t2 ON t2.n = t.n;
 -- ```delete from test where id IN (select ...``` - 
 -- ограничено лишь max_allowed_packet, так что можно и через IN
 
-SELECT 'Результат' as '===============================';
+SELECT 'Результат' as ' ';
 SELECT * FROM test;
--- mysql/join/join.sql
+++ mysql/join/join.sql
@@ -25,24 +25,24 @@ INSERT INTO `b` VALUES (1), (3);
 -- к каждой строке левой таблицы присоединяется каждая строка правой таблицы.
 -- 
 
--- INNER JOIN
+select 'INNER JOIN (внутреннее объединение)' as '';
 select * from a INNER JOIN b on b.b_id = a.a_id;
 
--- LEFT OUTER JOIN | LEFT JOIN
+select 'LEFT OUTER JOIN | LEFT JOIN' as '';
 select * from a LEFT JOIN b on b.b_id = a.a_id;
 select * from a LEFT OUTER JOIN b on b.b_id = a.a_id;
 
--- RIGHT OUTER JOIN | RIGHT JOIN
+select 'RIGHT OUTER JOIN | RIGHT JOIN' as '';
 select * from a RIGHT JOIN b on b.b_id = a.a_id;
 select * from a RIGHT OUTER JOIN b on b.b_id = a.a_id;
 
--- FULL OUTER JOIN
+select 'FULL OUTER JOIN' as '';
 select * from a LEFT JOIN b on b.b_id = a.a_id
 UNION
 select * from a RIGHT JOIN b on b.b_id = a.a_id;
 
--- CROSS JOIN
+select 'CROSS JOIN' as '';
 select * from a CROSS JOIN b;
 
--- FROM
+select 'FROM' as '';
 select * from a, b;
