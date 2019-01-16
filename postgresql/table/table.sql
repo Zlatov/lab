0de23859e01aa688260d59e9d54dd7ae501b7cf6
iadfeshchm <zlatov@ya.ru>
2019-01-16 20:17:19 +0300

    q

--- postgresql/table/table.sql
+++ postgresql/table/table.sql
@@ -9,11 +9,15 @@ DROP TABLE IF EXISTS lorem;
 CREATE TABLE lorem (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL UNIQUE,
+  code VARCHAR NULL UNIQUE,
+  blue BOOLEAN NOT NULL,
   age INT NOT NULL DEFAULT 18,
   CHECK (age > 0),
   address CHAR(50),
-  salary REAL
+  salary REAL,
+  UNIQUE (code, age)
 );
+CREATE INDEX ix_lorem_unique ON stat_stat (code, age);
 
 -- структура таблицы table.
 \d lorem
@@ -26,5 +30,12 @@ CREATE TABLE lorem (
 -- список всех таблиц, содержащих s в имени.
 \dt *s*
 
-INSERT INTO lorem (id, name, address) VALUES (DEFAULT, 'name1', 'address1');
-SELECT * FROM lorem LIMIT 1;
+INSERT INTO lorem (id, name, blue, code, address) VALUES
+(DEFAULT, 'name1', TRUE, NULL, 'address1'),
+(DEFAULT, 'name2', TRUE, NULL, 'address1'), -- Может быть две строки с NULL даже если поле UNIQUE так как NULL != NULL - факт.
+(DEFAULT, 'name3', FALSE, 'a1', 'address1'),
+(DEFAULT, 'name4', FALSE, 'a11', 'address1');
+
+SELECT * FROM lorem LIMIT 4;
+
+SELECT * FROM lorem where "code" IS NULL AND "age" = 18; -- Две записи, хотя code, age - составной уникальный индекс.
--- ruby/date/date.rb
+++ ruby/date/date.rb
@@ -1,13 +1,42 @@
 # encoding: UTF-8
-require 'rubygems'
+require 'active_support/all'
 require 'awesome_print'
 require 'date'
 
+puts 'DateTime'.green
 a = DateTime.new()
 print 'a: '.red; puts a
 a = DateTime.new(2001,2,3,4,5,6)
 print 'a: '.red; puts a
-
-a = Time.now
-print 'a: '.red; puts a.to_s
+a = DateTime.now
+print 'a: '.red; puts a
+print 'a.hour: '.red; puts a.hour
+print 'a.zone: '.red; puts a.zone
 print 'a.strftime("%m/%d/%Y"): '.red; puts a.strftime("%m/%d/%Y")
+
+puts 'Time'.green
+a = Time.now
+print 'a: '.red; puts a
+print 'a.hour: '.red; puts a.hour
+print 'a.zone: '.red; puts a.zone
+print 'a.strftime("%m/%d/%Y"): '.red; puts a.strftime("%m/%d/%Y")
+
+puts 'Арифметика'.blue
+puts 'Разность дат DateTime'.green
+a = DateTime.new 2000, 1, 1
+b = DateTime.new 2000, 1, 2
+c = b - a
+print 'c: '.red; puts c
+c = a - b
+print 'c.to_i: '.red; puts c.to_i
+puts 'Разность дат Time'.green
+a = Time.new 2000, 1, 1
+b = Time.new 2000, 1, 2
+c = b - a
+print 'c: '.red; puts c
+c = a - b
+print 'c.to_i: '.red; puts c.to_i
+
+puts 'Использование activesupport с DateTime'.green
+a = DateTime.new(2000,1,1) - 1.day
+print 'a: '.red; puts a
