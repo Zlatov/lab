3dbd1a26480c06e94f69a4bbcbf0e9412b0b0b02
iadfeshchm <zlatov@ya.ru>
2019-01-17 13:30:18 +0300

    q

--- ruby/const/const.rb
+++ ruby/const/const.rb
@@ -5,7 +5,13 @@ require 'awesome_print'
 class Cl
   CONST = '1'
   print 'self.CONST: '.red; puts self::CONST
+  def self.c
+    CONST
+  end
 end
 
 a = Cl::CONST
+print 'a: '.red; puts a
+
+a = Cl.c
 print 'a: '.red; puts a
