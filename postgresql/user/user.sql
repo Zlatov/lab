d173a69a50b3fdcee2b7fd258630479da6b79e55
iadfeshchm <zlatov@ya.ru>
2019-01-17 14:43:17 +0300

    q

--- postgresql/user/user.sql
+++ postgresql/user/user.sql
@@ -13,7 +13,7 @@
 -- 
 -- Добавлять пользователей в систему, настраивать тип авторизации пользователей
 -- 
--- sudo su root subl /etc/postgresql/9.3/main/pg_hba.conf
+-- sudo subl -n /etc/postgresql/9.3/main/pg_hba.conf
 -- Шаблон конфигурационной строки для настройки авторизации пользователей:
 -- local     DATABASE  USER      METHOD  [OPTIONS]
 -- Примеры:
