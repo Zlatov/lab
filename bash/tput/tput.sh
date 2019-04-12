5996c7be6f954b7d74bb17132806965f1da247d2
zlatov <zlatov@ya.ru>
2019-04-12 11:18:45 +0300

    bash tput: view how script running

--- bash/tput/tput.sh
+++ bash/tput/tput.sh
@@ -6,12 +6,22 @@ cd "$(dirname "${0}")"
 
 . ../_lib/echoc
 
-exit 0
+echo "hi"
+echo "hi"
+sleep 1
 
+# Очистить весь экран (или область устройства).
 tput clear >/dev/tty
+sleep 1
+
+echo "hi"
+sleep 1
+exit
+
 
 tput rmcup >/dev/tty
 tput smcup >/dev/tty
 
+# Сохранить текущую позицию (save cursor).
 tput sc >/dev/tty
 tput rc >/dev/tty
