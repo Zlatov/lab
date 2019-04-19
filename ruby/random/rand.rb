be2bdeb2ce3f1c09071afc0b4778dcc370d67e20
zlatov <zlatov@ya.ru>
2019-04-19 14:10:53 +0300

    q

---
+++ ruby/random/rand.rb
@@ -0,0 +1,4 @@
+require 'awesome_print'
+
+a = rand 10 # 0..9
+print 'a: '.red; puts a
---
+++ ruby/times/times.rb
@@ -0,0 +1,11 @@
+require 'awesome_print'
+
+2.times do
+  print 'asd'.green # asdasd
+end
+puts
+
+2.times do |i|
+  print "#{i}".green # 01
+end
+puts
