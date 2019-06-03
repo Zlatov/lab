af02fe1c247b5acb25dcf10b354149193415a99b
zlatov <zlatov@ya.ru>
2019-06-03 08:44:02 +0300

    q

--- bash/set/command.sh
+++ bash/set/command.sh
@@ -4,6 +4,8 @@ echo "---"
 echo "In command env:"
 echo '$A: ' $A
 echo '$B: ' $B
+echo '$C: ' $C
+echo '$D: ' $D
 echo '$DEVELOPER_HOST: ' $DEVELOPER_HOST
 echo '$HOME: ' $HOME
 echo ~
--- bash/set/command_clean_env.sh
+++ bash/set/command_clean_env.sh
@@ -2,16 +2,22 @@
 
 A=AAA
 
+export D='DDD' # будет видна в ./command.sh
+
 set -a
 
-. ./var.sh
+. ./var.sh # назначенные тут переменные будут видны в ./command.sh (будут экспортированы)
 
 set +a
+
+. ./var2.sh # `С` не будет видна в ./command.sh
 
 echo "---"
 echo "In clean env:"
 echo '$A: ' $A
 echo '$B: ' $B
+echo '$C: ' $C
+echo '$D: ' $D
 echo '$DEVELOPER_HOST: ' $DEVELOPER_HOST
 echo '$HOME: ' $HOME
 echo ~
