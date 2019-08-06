09b15cd1228f93f956bd449960e06ab9a21d7aeb
zlatov <zlatov@ya.ru>
2019-08-06 19:38:40 +0300

    q

--- linux/service/service.sh
+++ linux/service/service.sh
@@ -1,5 +1,11 @@
-
+exit 0
 # 
 # sudo service nginx stop
 # sudo systemctl disable nginx
 # 
+
+# Список сервисов Centos
+systemctl
+systemctl | grep httpd
+systemctl list-units --type service
+systemctl list-units --type mount
