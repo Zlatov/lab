6944588aebabe116882f43c836f87cc3c543d553
zlatov <zlatov@ya.ru>
2019-11-15 19:26:31 +0300

    q

---
+++ rails/_tasks/generate_account/generate_account.md
@@ -0,0 +1,4 @@
+```bash
+# --no-controller-specs --no-view-specs
+TASKER=1 rails g controller market/desktop/vision/account client_profile order_history account_settings add_client --no-helper --no-assets --no-test-framework
+```
--- rails/routes/routes.rb
+++ rails/routes/routes.rb
@@ -1,15 +1,19 @@
 
 # 
 # `$ rails routes` - список всех путей;
-# `> app.products_path` - проверка пути, вернёт url;
+# `$ rails routes | grep account` - список путей которые содержат ...;
+# `> app.products_path` - проверка пути, вернёт url относительно домена;
 # `> app.product_path 123` - -//-
+# `> app.products_url` - проверка пути, вернёт Абсолютный url (с http).
 # 
 
 get "lorem/grid"
 
 get "lorem/grid" => "lorem#grid"
 
-get "lorem/grid" => "lorem#grid", as: "lorem_grid"
+# Сразу как :id добавлено не формируется автоматический строковый идентификатор пути,
+# поэтому добавляем as: '...'
+get "lorem/grid/:id" => "lorem#grid", as: "lorem_grid"
 
 scope module: "market/desktop/vision" do
   get "lorem/grid"
