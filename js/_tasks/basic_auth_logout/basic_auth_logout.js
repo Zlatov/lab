a3bc62a8d027190745a3faa568c88fa8298dcccd
zlatov <zlatov@ya.ru>
2020-07-31 21:21:09 +0300

    q

---
+++ js/_tasks/basic_auth_logout/basic_auth_logout.js
@@ -0,0 +1,22 @@
+// В хроме работает и так и так, а вот в FF не срабатывает.
+function logout() {
+  $.ajax({
+    type: "GET",
+    url: "/",
+    async: false,
+    // username: "logmeout",
+    // password: "123456",
+    // headers: { "Authorization": "Basic xxx" }
+    beforeSend: function (jqXHR, settings) {
+      jqXHR.setRequestHeader("Authorization", "Basic " + btoa("logmeout:123456"))
+    }
+  })
+  .done(function(data) {
+    console.log('data: ', data)
+  })
+  .fail(function(error) {
+    console.log('error: ', error)
+    window.location = "/"
+  });
+  return false
+}
--- rails/controller/controller.rb
+++ rails/controller/controller.rb
@@ -15,6 +15,20 @@ class NewsController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @catalog_order.update(catalog_order_params)
+        format.html do
+          redirect_to action: :index, form_success: 'Catalog order was successfully updated.'
+        end
+        format.json { render :show, status: :ok, location: @catalog_order }
+      else
+        format.html { render :edit }
+        format.json { render json: @catalog_order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
   def find_model
     @model = News.find(params[:id]) if params[:id]
@@ -40,5 +54,14 @@ class NewsController < ApplicationController
     else
       "application"
     end
+  end
+end
+
+# app/controllers/posts_controller.rb
+class PostsController < ApplicationController
+  respond_to :json
+
+  def show
+    respond_with Post.find(params[:id])
   end
 end
