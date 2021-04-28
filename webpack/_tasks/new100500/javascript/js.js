6e9ece082142a3da37e4fbabd6b0d8efc681c581
zlatov <zlatov@ya.ru>
2021-04-28 22:51:45 +0300

    q

---
+++ webpack/_tasks/new100500/javascript/js.js
@@ -0,0 +1,18 @@
+import $ from "jquery"
+global.$ = $
+global.jQuery = $
+
+import "bootstrap"
+
+import "ilorem"
+
+import "holderjs"
+
+import "./sass.scss"
+
+// Импорт изображений
+import "./images/other_sources/github.svg"
+import "./images/other_sources/npmjs.svg"
+import "./images/favicon.svg"
+import "./images/ilorem.svg"
+import "./images/ilorem-white.svg"
