c9ff1af3afc438dbf6a0e747110e5fbd728d964c
zlatov <zlatov@ya.ru>
2021-05-05 23:12:07 +0300

    start chart.js with webpack

---
+++ js/_tasks/chart.js/javascript/js.js
@@ -0,0 +1,25 @@
+import $ from "jquery"
+global.$ = $
+global.jQuery = $
+
+import "bootstrap"
+
+import "ilorem"
+
+import "holderjs"
+
+import "./js/chart.js"
+
+import "./sass.scss"
+
+// Импорт изображений
+import "./images/logo.svg"
+import "./images/favicon.svg"
+
+;(function($) {
+  "use strict"
+
+  $(document).ready(function() {
+    window.chart.activate()
+  });
+})(jQuery);
