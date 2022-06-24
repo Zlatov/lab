988c66ff4c6109a1ddd69b95190b9f4ea8022d08
iadfeshchm <zlatov@ya.ru>
2022-06-24 17:58:34 +0300

    q

--- git/remote/remote.sh
+++ git/remote/remote.sh
@@ -6,5 +6,14 @@ git remote -v # Показать хранилища и их url-ы
 git remote set-url origin https://github.com/USERNAME/REPOSITORY.git
 
 # Или
-git remote remove origin
+git remote remove old-origin
 git remote add origin <url>
+
+# Перенос в другой гит репозиторий
+git remote rename origin old-origin
+git remote add origin git@gitlab.newstar.ru:gitlab-instance-2d4cb74e/zenonline-admin.git
+git push -u origin --all
+git push -u origin --tags
+
+# Сменить какую удалённую ветку отслеживает локальная
+git branch --set-upstream-to=origin/master master
