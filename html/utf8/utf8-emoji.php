048dbbedffafd6856c9b3fb4d64861c68478e96c
iadfeshchm <zlatov@ya.ru>
2023-06-30 19:06:12 +0300

    q

---
+++ html/utf8/utf8-emoji.php
@@ -0,0 +1,99 @@
+<body>
+<p>1F601 - &#x1F601; - <?php echo hexdec('1F601'); ?> - &#<?php echo hexdec('1F601'); ?>;</p>
+<p>1F602 - &#x1F602; - <?php echo hexdec('1F602'); ?> - &#<?php echo hexdec('1F602'); ?>;</p>
+<p>1F603 - &#x1F603; - <?php echo hexdec('1F603'); ?> - &#<?php echo hexdec('1F603'); ?>;</p>
+<p>1F604 - &#x1F604; - <?php echo hexdec('1F604'); ?> - &#<?php echo hexdec('1F604'); ?>;</p>
+<p>1F605 - &#x1F605; - <?php echo hexdec('1F605'); ?> - &#<?php echo hexdec('1F605'); ?>;</p>
+<p>1F606 - &#x1F606; - <?php echo hexdec('1F606'); ?> - &#<?php echo hexdec('1F606'); ?>;</p>
+<p>1F609 - &#x1F609; - <?php echo hexdec('1F609'); ?> - &#<?php echo hexdec('1F609'); ?>;</p>
+<p>1F60A - &#x1F60A; - <?php echo hexdec('1F60A'); ?> - &#<?php echo hexdec('1F60A'); ?>;</p>
+<p>1F60B - &#x1F60B; - <?php echo hexdec('1F60B'); ?> - &#<?php echo hexdec('1F60B'); ?>;</p>
+<p>1F60C - &#x1F60C; - <?php echo hexdec('1F60C'); ?> - &#<?php echo hexdec('1F60C'); ?>;</p>
+<p>1F60D - &#x1F60D; - <?php echo hexdec('1F60D'); ?> - &#<?php echo hexdec('1F60D'); ?>;</p>
+<p>1F60F - &#x1F60F; - <?php echo hexdec('1F60F'); ?> - &#<?php echo hexdec('1F60F'); ?>;</p>
+<p>1F612 - &#x1F612; - <?php echo hexdec('1F612'); ?> - &#<?php echo hexdec('1F612'); ?>;</p>
+<p>1F613 - &#x1F613; - <?php echo hexdec('1F613'); ?> - &#<?php echo hexdec('1F613'); ?>;</p>
+<p>1F614 - &#x1F614; - <?php echo hexdec('1F614'); ?> - &#<?php echo hexdec('1F614'); ?>;</p>
+<p>1F616 - &#x1F616; - <?php echo hexdec('1F616'); ?> - &#<?php echo hexdec('1F616'); ?>;</p>
+<p>1F618 - &#x1F618; - <?php echo hexdec('1F618'); ?> - &#<?php echo hexdec('1F618'); ?>;</p>
+<p>1F61A - &#x1F61A; - <?php echo hexdec('1F61A'); ?> - &#<?php echo hexdec('1F61A'); ?>;</p>
+<p>1F61C - &#x1F61C; - <?php echo hexdec('1F61C'); ?> - &#<?php echo hexdec('1F61C'); ?>;</p>
+<p>1F61D - &#x1F61D; - <?php echo hexdec('1F61D'); ?> - &#<?php echo hexdec('1F61D'); ?>;</p>
+<p>1F61E - &#x1F61E; - <?php echo hexdec('1F61E'); ?> - &#<?php echo hexdec('1F61E'); ?>;</p>
+<p>1F620 - &#x1F620; - <?php echo hexdec('1F620'); ?> - &#<?php echo hexdec('1F620'); ?>;</p>
+<p>1F621 - &#x1F621; - <?php echo hexdec('1F621'); ?> - &#<?php echo hexdec('1F621'); ?>;</p>
+<p>1F622 - &#x1F622; - <?php echo hexdec('1F622'); ?> - &#<?php echo hexdec('1F622'); ?>;</p>
+<p>1F623 - &#x1F623; - <?php echo hexdec('1F623'); ?> - &#<?php echo hexdec('1F623'); ?>;</p>
+<p>1F624 - &#x1F624; - <?php echo hexdec('1F624'); ?> - &#<?php echo hexdec('1F624'); ?>;</p>
+<p>1F625 - &#x1F625; - <?php echo hexdec('1F625'); ?> - &#<?php echo hexdec('1F625'); ?>;</p>
+<p>1F628 - &#x1F628; - <?php echo hexdec('1F628'); ?> - &#<?php echo hexdec('1F628'); ?>;</p>
+<p>1F629 - &#x1F629; - <?php echo hexdec('1F629'); ?> - &#<?php echo hexdec('1F629'); ?>;</p>
+<p>1F62A - &#x1F62A; - <?php echo hexdec('1F62A'); ?> - &#<?php echo hexdec('1F62A'); ?>;</p>
+<p>1F62B - &#x1F62B; - <?php echo hexdec('1F62B'); ?> - &#<?php echo hexdec('1F62B'); ?>;</p>
+<p>1F62D - &#x1F62D; - <?php echo hexdec('1F62D'); ?> - &#<?php echo hexdec('1F62D'); ?>;</p>
+<p>1F630 - &#x1F630; - <?php echo hexdec('1F630'); ?> - &#<?php echo hexdec('1F630'); ?>;</p>
+<p>1F631 - &#x1F631; - <?php echo hexdec('1F631'); ?> - &#<?php echo hexdec('1F631'); ?>;</p>
+<p>1F632 - &#x1F632; - <?php echo hexdec('1F632'); ?> - &#<?php echo hexdec('1F632'); ?>;</p>
+<p>1F633 - &#x1F633; - <?php echo hexdec('1F633'); ?> - &#<?php echo hexdec('1F633'); ?>;</p>
+<p>1F635 - &#x1F635; - <?php echo hexdec('1F635'); ?> - &#<?php echo hexdec('1F635'); ?>;</p>
+<p>1F637 - &#x1F637; - <?php echo hexdec('1F637'); ?> - &#<?php echo hexdec('1F637'); ?>;</p>
+<p>1F638 - &#x1F638; - <?php echo hexdec('1F638'); ?> - &#<?php echo hexdec('1F638'); ?>;</p>
+<p>1F639 - &#x1F639; - <?php echo hexdec('1F639'); ?> - &#<?php echo hexdec('1F639'); ?>;</p>
+<p>1F63A - &#x1F63A; - <?php echo hexdec('1F63A'); ?> - &#<?php echo hexdec('1F63A'); ?>;</p>
+<p>1F63B - &#x1F63B; - <?php echo hexdec('1F63B'); ?> - &#<?php echo hexdec('1F63B'); ?>;</p>
+<p>1F63C - &#x1F63C; - <?php echo hexdec('1F63C'); ?> - &#<?php echo hexdec('1F63C'); ?>;</p>
+<p>1F63D - &#x1F63D; - <?php echo hexdec('1F63D'); ?> - &#<?php echo hexdec('1F63D'); ?>;</p>
+<p>1F63E - &#x1F63E; - <?php echo hexdec('1F63E'); ?> - &#<?php echo hexdec('1F63E'); ?>;</p>
+<p>1F63F - &#x1F63F; - <?php echo hexdec('1F63F'); ?> - &#<?php echo hexdec('1F63F'); ?>;</p>
+<p>1F640 - &#x1F640; - <?php echo hexdec('1F640'); ?> - &#<?php echo hexdec('1F640'); ?>;</p>
+<p>1F645 - &#x1F645; - <?php echo hexdec('1F645'); ?> - &#<?php echo hexdec('1F645'); ?>;</p>
+<p>1F646 - &#x1F646; - <?php echo hexdec('1F646'); ?> - &#<?php echo hexdec('1F646'); ?>;</p>
+<p>1F647 - &#x1F647; - <?php echo hexdec('1F647'); ?> - &#<?php echo hexdec('1F647'); ?>;</p>
+<p>1F648 - &#x1F648; - <?php echo hexdec('1F648'); ?> - &#<?php echo hexdec('1F648'); ?>;</p>
+<p>1F649 - &#x1F649; - <?php echo hexdec('1F649'); ?> - &#<?php echo hexdec('1F649'); ?>;</p>
+<p>1F64A - &#x1F64A; - <?php echo hexdec('1F64A'); ?> - &#<?php echo hexdec('1F64A'); ?>;</p>
+<p>1F64B - &#x1F64B; - <?php echo hexdec('1F64B'); ?> - &#<?php echo hexdec('1F64B'); ?>;</p>
+<p>1F64C - &#x1F64C; - <?php echo hexdec('1F64C'); ?> - &#<?php echo hexdec('1F64C'); ?>;</p>
+<p>1F64D - &#x1F64D; - <?php echo hexdec('1F64D'); ?> - &#<?php echo hexdec('1F64D'); ?>;</p>
+<p>1F64E - &#x1F64E; - <?php echo hexdec('1F64E'); ?> - &#<?php echo hexdec('1F64E'); ?>;</p>
+<p>1F64F - &#x1F64F; - <?php echo hexdec('1F64F'); ?> - &#<?php echo hexdec('1F64F'); ?>;</p>
+<p>1F600 - &#x1F600; - <?php echo hexdec('1F600'); ?> - &#<?php echo hexdec('1F600'); ?>;</p>
+<p>1F607 - &#x1F607; - <?php echo hexdec('1F607'); ?> - &#<?php echo hexdec('1F607'); ?>;</p>
+<p>1F608 - &#x1F608; - <?php echo hexdec('1F608'); ?> - &#<?php echo hexdec('1F608'); ?>;</p>
+<p>1F60E - &#x1F60E; - <?php echo hexdec('1F60E'); ?> - &#<?php echo hexdec('1F60E'); ?>;</p>
+<p>1F610 - &#x1F610; - <?php echo hexdec('1F610'); ?> - &#<?php echo hexdec('1F610'); ?>;</p>
+<p>1F611 - &#x1F611; - <?php echo hexdec('1F611'); ?> - &#<?php echo hexdec('1F611'); ?>;</p>
+<p>1F615 - &#x1F615; - <?php echo hexdec('1F615'); ?> - &#<?php echo hexdec('1F615'); ?>;</p>
+<p>1F617 - &#x1F617; - <?php echo hexdec('1F617'); ?> - &#<?php echo hexdec('1F617'); ?>;</p>
+<p>1F619 - &#x1F619; - <?php echo hexdec('1F619'); ?> - &#<?php echo hexdec('1F619'); ?>;</p>
+<p>1F61B - &#x1F61B; - <?php echo hexdec('1F61B'); ?> - &#<?php echo hexdec('1F61B'); ?>;</p>
+<p>1F61F - &#x1F61F; - <?php echo hexdec('1F61F'); ?> - &#<?php echo hexdec('1F61F'); ?>;</p>
+<p>1F626 - &#x1F626; - <?php echo hexdec('1F626'); ?> - &#<?php echo hexdec('1F626'); ?>;</p>
+<p>1F627 - &#x1F627; - <?php echo hexdec('1F627'); ?> - &#<?php echo hexdec('1F627'); ?>;</p>
+<p>1F62C - &#x1F62C; - <?php echo hexdec('1F62C'); ?> - &#<?php echo hexdec('1F62C'); ?>;</p>
+<p>1F62E - &#x1F62E; - <?php echo hexdec('1F62E'); ?> - &#<?php echo hexdec('1F62E'); ?>;</p>
+<p>1F62F - &#x1F62F; - <?php echo hexdec('1F62F'); ?> - &#<?php echo hexdec('1F62F'); ?>;</p>
+<p>1F634 - &#x1F634; - <?php echo hexdec('1F634'); ?> - &#<?php echo hexdec('1F634'); ?>;</p>
+<p>1F636 - &#x1F636; - <?php echo hexdec('1F636'); ?> - &#<?php echo hexdec('1F636'); ?>;</p>
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ telegram/bot/bot_api.md
@@ -0,0 +1,5 @@
+
+```sh
+# Отправить сообщение
+curl -X POST -H "Content-Type: application/json" -d "{\"chat_id\": \"<CHAT_ID>\", \"text\": \"Hi\"}" https://api.telegram.org/bot<BOT_TOKEN>/sendMessage
+```
