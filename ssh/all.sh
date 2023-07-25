a0d4af4671c9c9aa897d1b9cf36b13acc036f6b9
zlatov <zlatov@ya.ru>
2023-07-25 11:56:15 +0300

    q

--- ssh/all.sh
+++ ssh/all.sh
@@ -14,7 +14,7 @@ ssh-copy-id -i ~/.ssh/id_rsa.pub myserv
 # Принудительно запрашивать пароль
 ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no user@host
 
-# Принудительно запрашивать пароль
+# 
 sudo apt install -y openssh-server
 sudo service ssh status
 
