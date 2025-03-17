22d1a27d54b8a88baf82c85d6ef92f0df21f2fcf
zlatov <zlatov@ya.ru>
2025-03-17 11:49:46 +0300

    q

--- ruby/puts_print_p/print_console.rb
+++ ruby/puts_print_p/print_console.rb
@@ -7,6 +7,15 @@
 
 require 'awesome_print'
 
+array = (0..99).to_a
+count = array.length
+last_index = count -1
+array.each.with_index do |value, index|
+  sleep 0.01
+  printf "%s/%s#{index == last_index ? "\n" : "\r"}", index + 1, count
+end
+# exit
+
 puts 'Показываем прогрес выполнения задачи выполненными шагами'.green
 a = (1..1000).to_a
 b = a.length
@@ -32,7 +41,7 @@ puts 'Показываем прогрес выполнения задачи за
 a = (0..99).to_a
 a.each_with_index do |c, index|
   percent = (index.to_f/a.length*100).floor
-  printf "\rВыполнено %s", '[' + '=' * percent + '-' * (100 - percent) + ']'
+  printf "Выполнено %s\r", '[' + '=' * percent + '-' * (100 - percent) + ']'
   sleep 0.01
 end
 puts "\rВыполнено [" + '=' * 100 + "]"
